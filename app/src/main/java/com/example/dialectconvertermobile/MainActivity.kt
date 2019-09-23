8e97ce4e174052a987b838cedeaa6340ddb66beb
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-09-23 18:28:30 +0900

    パース処理をMecab仕様からKuromoji仕様へ変更

--- app/src/main/java/com/example/dialectconvertermobile/MainActivity.kt
+++ app/src/main/java/com/example/dialectconvertermobile/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.dialectconvertermobile
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.atilika.kuromoji.ipadic.Tokenizer
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,15 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val inputText = "お爺ちゃんあそこにおいてあるペン取って"
-        val tokenizer = Tokenizer()
-        for(token in tokenizer.tokenize(inputText)) {
-            Log.d("token", "${token}")
-        }
-//        val command = arrayOf(
-//            "sh", "-c",
-//            "echo ${inputText} | mecab -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd"
-//        )
-//        val parsedDataList = Parser().parse(command)
+        val parsedDataList = Parser().parse(inputText)
 //        Converter().convert(parsedDataList)
     }
 }
