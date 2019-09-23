7cdfb8afd9836afe1d6244407221e273d0e44710
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-09-23 19:30:38 +0900

    メイン画面にて、変換ボタンをイベントトリガとして標準語を読み込み、遠州弁を出力する処理を実装

--- app/src/main/java/com/example/dialectconvertermobile/MainActivity.kt
+++ app/src/main/java/com/example/dialectconvertermobile/MainActivity.kt
@@ -2,17 +2,28 @@ package com.example.dialectconvertermobile
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import org.dom4j.io.SAXReader
 
 class MainActivity : AppCompatActivity() {
 
+    // TODO:遠州弁の強さをモード制御
+    // TODO:テキストファイルを読み込みテキストファイルに出力する処理
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val inputText = "お爺ちゃんあそこにおいてあるペン取って"
-        val parsedDataList = Parser().parse(inputText)
-        val reader = SAXReader()
-        val document = reader.read(assets.open("dialect_data.xml"))
-        Converter(document).convert(parsedDataList)
+        val executeButton = findViewById<View>(R.id.executeButton)
+
+        executeButton.setOnClickListener {
+            val inputText = findViewById<View>(R.id.inputText) as EditText
+            val parsedDataList = Parser().parse("${inputText.text}")
+            val reader = SAXReader()
+            val document = reader.read(assets.open("dialect_data.xml"))
+            val outputText = findViewById<View>(R.id.outputText) as TextView
+            Converter(document).convert(parsedDataList).forEach { outputText.append(it) }
+        }
     }
 }
